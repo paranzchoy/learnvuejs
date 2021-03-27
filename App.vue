83b920b8c6643f46585eaee5bb6c24f509498095
Franz Duran <engrparanz@gmail.com>
2021-03-27 09:26:39 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+        <button v-on:click="reverseMessage">
+            Reverse
+        </button>
+        <!-- <p>{{ reverseMessage }}</p> -->
+        <p>Counter: {{ counter }}</p>
+
+        <!-- <ul>
+        <todo-item
+            v-for="item in groceryList"
+            v-bind:todo="item"
+            v-bind:key="item.id"
+        ></todo-item>
+        </ul> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "PhilBest",
+            blah: "nyaha",
+            counter: 0,
+            groceryList: [
+                { id: 1, text: 'Javascript'},
+                { id: 2, text: 'React'},
+                { id: 3, text: 'Vue'},
+                { id: 4, text: 'Blazor'},
+            ]
+        }
+    },
+    methods: {
+        reverseMessage() {
+            this.message = this.message
+            .split('')
+            .reverse()
+            .join('')
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.counter++
+        }, 1000)
+    }          
+}
+</script>
+
+<style>
+    #app_div {
+        font-family: Helvetica, Arial, sans-serif;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ main.js
@@ -0,0 +1,6 @@
+// import App from './App.vue';
+const App = require('./App.vue');
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app_div')
